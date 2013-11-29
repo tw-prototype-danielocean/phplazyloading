69fbb6e558064d052acc91b74be3cccd4306ee46
ocean <ocean@192.168.2.90>
2013-11-30 03:21:06 +0800

    init lazyloading

--- lazyloading.php
+++ lazyloading.php
@@ -1,3 +1,62 @@
-<?php
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>lazyloading</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <script src="js/holder.js"></script>
+    </head>
+    <body>
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+        <img class="lazy" data-original="holder.js/200x200/" width="200" height="200">
+    </body>
 
-?>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="js/jquery.lazyload.js"></script>
+    <script type="text/javascript" charset="utf-8">
+        $(function() {
+            $("img.lazy").lazyload({
+                effect: "fadeIn"
+            });
+        });
+    </script>
+</html>
